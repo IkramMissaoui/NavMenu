553390b6283e97e7ad76adb3adf6e200a55810fd
IkramMissaoui <missaoui.ikram726@gmail.com>
2019-05-20 12:19:55 +0200

    Updates

---
+++ precache-manifest.bfe5d8962090b7e5b426fdebd0b24077.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4459d295023eceaeddb70c2ca502584",
+    "url": "/NavMenu/index.html"
+  },
+  {
+    "revision": "ad0915249a7673098708",
+    "url": "/NavMenu/static/css/main.0a092ed9.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/NavMenu/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ad0915249a7673098708",
+    "url": "/NavMenu/static/js/main.ca86529e.chunk.js"
+  },
+  {
+    "revision": "c61e3df1bb422c4a1704",
+    "url": "/NavMenu/static/js/runtime~main.ab7cf805.js"
+  }
+]);
